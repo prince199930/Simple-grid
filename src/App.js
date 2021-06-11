93c5937f31a452b3360f1858ec9a6fb7a17d2280
prince199930 <princehira199930@gamil.com>
2021-06-11 13:49:07 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
+import React, { Component,PureComponent, useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Parent = () => {
+  const [buttons , setButtons] = useState([]);
+  const [btnClick , setBtnClick] = useState(null);
+  const [cards,updateCards] = useState([]);
+  const [searchValue,setSearchValue] = useState('')
+  const [orginalData,setOrginalData]=useState([])
+  useEffect(()=>{
+    axios.get('https://api.edyoda.com/v1/blog/postcategories/')
+  .then(json => {
+    setButtons(json.data)
+  })
+
+  axios.get('https://api.edyoda.com/v1/blog/')
+  .then(blog => {
+    setOrginalData(blog.data)
+    updateCards(blog.data)
+  })},[])
+
+  const onSearch = ()=>{
+    const updateValues = orginalData.filter((item)=>
+    item.authorname.toLowerCase().includes(searchValue.toLowerCase())||item.slug.toLowerCase().includes(searchValue.toLowerCase()))
+    updateCards(updateValues)
+    
+  }
+  
+  useEffect(()=>{
+    console.log('component did update') 
+  })
+  const btncards=(id)=>{
+    setBtnClick(id)
+    const btnOnClick = orginalData.filter((item)=>item.id == id)
+    updateCards(btnOnClick);
+  }
+  return (<div>
+    <input type="search" placeholder="search.." id="search-box" onBlur={(e)=>e.target.value=''} onChange={(e)=>setSearchValue(e.target.value)}/><button onClick={onSearch}>search</button>
+    <div>
+    {buttons.map((item)=>
+      <div className={btnClick === item.id ? "buttons active":"buttons"} onClick={()=>{btncards(item.id)}} key={item.id}>{item.title}</div>)}</div>
+     <div className="cardWrap">
+      { cards.map(({authorname,description,id,slug,small_image,title})=>
+      <div className="card">
+        <img src={small_image}/>
+        <h5>{title}</h5>
+        <p>{authorname}</p>
+        <p>{description}</p>
+      </div>
+      
+      )}</div>
+    
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+ 
+export default Parent;
